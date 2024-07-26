0604e2a36524fea97a88ebb1ae34594494299774
Christopher Alexman <christopheralexman@gmail.com>
2024-07-26 01:04:20 -0700

    Change loop for all images, add comment for aspect ratio reduction

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # image-resizer
 
-A basic script that takes in a .jpg file and crops it to the 4:3 aspect ratio.
+A basic script that takes in a .jpg file and crops it closer to the 4:3 aspect ratio.
 It considers whitespace along the image border for what to remove when cropping.
 
 ### INPUT
-- .jpg file in landscape orientation
+- .jpg file
 - height range: between 709 and 777
 - width range: 1023 or 1024
 
 ### OUTPUT
-- .jpg file at the 4:3 aspect ratio
+- .jpg file as close to the 4:3 aspect ratio as possible
+
+### SOURCES:
+- https://pillow.readthedocs.io/en/stable/reference/Image.html
+- https://www.mathworks.com/help/images/image-coordinate-systems.html
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-A basic script that takes in a .jpg file and crops it to the 4:3 aspect ratio.
+A basic script that takes in a .jpg file and crops it closer to the 4:3 aspect ratio.
 It considers whitespace along the image border for what to remove when cropping.
 
 INPUT:
@@ -8,7 +8,7 @@ INPUT:
 - width range: 1023 or 1024
 
 OUTPUT:
-- .jpg file at the 4:3 aspect ratio
+- .jpg file as close to the 4:3 aspect ratio as possible
 
 SOURCES:
 - https://pillow.readthedocs.io/en/stable/reference/Image.html
@@ -30,118 +30,136 @@ def resize_images():
 
         image_path = f'{input_path}\{image}'
 
-        # consider image 001 for development
-        if(image == '001.jpg'):
+        with Image.open(image_path) as img:
 
-            with Image.open(image_path) as img:
+            # NOTE: images with landscape orientation will be rotated 90 degrees clockwise
+            # width is left/right, height is up/down
+            width, height = img.size
 
-                # NOTE: images with landscape orientation will be rotated 90 degrees clockwise
-                # width is left/right, height is up/down
-                width, height = img.size
-                print(f'w: {width} | h: {height}')
+            # 4:3 ratio || 1 / 1.33 = width / height
+            #   width = height / 1.33
+            #   height = 1.33 * width
+            width_ratio = round(height / 1.33)
 
-                # 4:3 ratio || 1 / 1.33 = width / height
-                #   width = height / 1.33
-                #   height = 1.33 * width
-                width_ratio = round(height / 1.33)
+            # set height & width to 4:3 ratio based on smaller side
+            if(width_ratio > width):
+                height_ratio = round(1.33 * width)
+                width_ratio = width
+            else:
+                height_ratio = height
 
-                # set height & width to 4:3 ratio based on smaller side
-                if(width_ratio > width):
-                    height_ratio = round(1.33 * width)
-                    width_ratio = width
-                else:
-                    height_ratio = height
-
-                '''
-                cartesian pixel coordinate system:
-                    - top left is (0,0)
-                    - pixel indices are [y, x]
+            '''
+            cartesian pixel coordinate system:
+                - top left is (0,0)
+                - pixel indices are [y, x]
 
                 (0,0) ___________ (x, 0)
-                        |           |
-                        |           |
-                        |___________|
+                    |           |
+                    |           |
+                    |___________|
                 (0, y)             (x, y)
-                '''
+            '''
 
-                pixel_img = img.load()
-                x = width - 1
-                y = height - 1
+            pixel_img = img.load()
+            x = width - 1
+            y = height - 1
 
-                left_x, left_y = [0, round(y / 2)]
-                right_x, right_y = [x, round(y / 2)]
-                top_x, top_y = [round(x / 2), 0]
-                bottom_x, bottom_y = [round(x / 2), y]
+            left_x, left_y = [0, round(y / 2)]
+            right_x, right_y = [x, round(y / 2)]
+            top_x, top_y = [round(x / 2), 0]
+            bottom_x, bottom_y = [round(x / 2), y]
 
-                threshold = (100, 100, 100)
+            threshold = (100, 100, 100)
 
-                left_remove = 0
-                right_remove = 0
-                top_remove = 0
-                bottom_remove = 0
+            left_remove = 0
+            right_remove = 0
+            top_remove = 0
+            bottom_remove = 0
 
-                black_pixels = 0
-                pixels_in_a_row = 3
+            black_pixels = 0
+            pixels_in_a_row = 3
 
-                for num in range(round(height / 8)):
-                    color = pixel_img[left_x + num, left_y]
-                    if threshold > color:
-                        black_pixels += 1
-                        if black_pixels > pixels_in_a_row:
-                            left_remove = num - pixels_in_a_row
-                            break
-                    else:
-                        black_pixels = 0
-                black_pixels = 0
+            for num in range(round(height / 8)):
+                color = pixel_img[left_x + num, left_y]
+                if threshold > color:
+                    black_pixels += 1
+                    if black_pixels > pixels_in_a_row:
+                        left_remove = num - pixels_in_a_row
+                        break
+                else:
+                    black_pixels = 0
+            black_pixels = 0
 
-                for num in range(round(height / 8)):
-                    color = pixel_img[right_x - num, right_y]
-                    if threshold > color:
-                        black_pixels += 1
-                        if black_pixels > pixels_in_a_row:
-                            right_remove = num - pixels_in_a_row
-                            break
-                    else:
-                        black_pixels = 0
-                black_pixels = 0
+            for num in range(round(height / 8)):
+                color = pixel_img[right_x - num, right_y]
+                if threshold > color:
+                    black_pixels += 1
+                    if black_pixels > pixels_in_a_row:
+                        right_remove = num - pixels_in_a_row
+                        break
+                else:
+                    black_pixels = 0
+            black_pixels = 0
 
-                for num in range(round(width / 8)):
-                    color = pixel_img[top_x, top_y + num]
-                    if threshold > color:
-                        black_pixels += 1
-                        if black_pixels > pixels_in_a_row:
-                            top_remove = num - pixels_in_a_row
-                            break
-                    else:
-                        black_pixels = 0
-                black_pixels = 0
+            for num in range(round(width / 8)):
+                color = pixel_img[top_x, top_y + num]
+                if threshold > color:
+                    black_pixels += 1
+                    if black_pixels > pixels_in_a_row:
+                        top_remove = num - pixels_in_a_row
+                        break
+                else:
+                    black_pixels = 0
+            black_pixels = 0
 
-                for num in range(round(width / 8)):
-                    color = pixel_img[bottom_x, bottom_y - num]
-                    if threshold > color:
-                        black_pixels += 1
-                        if black_pixels > pixels_in_a_row:
-                            bottom_remove = num - pixels_in_a_row
-                            break
-                    else:
-                        black_pixels = 0
-                black_pixels = 0
+            for num in range(round(width / 8)):
+                color = pixel_img[bottom_x, bottom_y - num]
+                if threshold > color:
+                    black_pixels += 1
+                    if black_pixels > pixels_in_a_row:
+                        bottom_remove = num - pixels_in_a_row
+                        break
+                else:
+                    black_pixels = 0
+            black_pixels = 0
 
-                print(f'left: {left_remove}')
-                print(f'right: {right_remove}')
-                print(f'top: {top_remove}')
-                print(f'bottom: {bottom_remove}')
+            new_width = 0
+            new_height = 0
+            smallest_ratio = height / width
+            candidate = []
 
-                left = left_remove
-                top = top_remove
-                right = width - right_remove
-                bottom = height - bottom_remove
+            for l in range(left_remove + 1):
+                for r in range(right_remove + 1):
 
-                # width_remove = left_remove + right_remove
-                # height_remove = top_remove + bottom_remove
+                    new_width = width - l - r
+
+                    for t in range(top_remove + 1):
+                        for b in range(bottom_remove + 1):
+
+                            new_height = height - b - t
+                            new_ratio = new_height / new_width
+
+                            if new_ratio < smallest_ratio and new_ratio > 1.33:
+                                smallest_ratio = new_ratio
+                                candidate = [l, r, t, b]
+
+            if candidate:
+                l, r, t, b = candidate
+                new_width = width - l - r
+                new_height = height - b - t
+
+                left = l
+                top = t
+                right = width - r
+                bottom = height - b
+
+                ratio_before = height / width
+                ratio_after = new_height / new_width
 
                 img = img.crop((left, top, right, bottom))
-                img.save(f'{output_path}\{image}')
+                print(f'reduced {image} | {ratio_before:.4f} -> {ratio_after:.4f}')
+
+        img.save(f'{output_path}\{image}')
 
 
 if __name__ == '__main__':
