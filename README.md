9c1d29e8c52e9f46cda2bed77976f5f759195bb8
Christopher Alexman <christopheralexman@gmail.com>
2024-07-28 16:55:56 -0700

    Add Image class for code organization and readability

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
-# image-resizer
+# Image Resizer
 
-A basic script that takes in a .jpg file and crops it closer to the 2.5 x 3.5 inch ratio of a trading card.
-It considers whitespace along the image border for what to remove when cropping.
+A basic script that crops .jpg files to be as close as possible to the 1.4:1 aspect ratio of trading cards.
+It considers whitespace along the image border for what to remove (this is a specific use case for my project).
 
-### INPUT
+## INPUT
 - .jpg file
 - height range: between 709 and 777
 - width range: 1023 or 1024
 
-### OUTPUT
-- .jpg file as close to the 2.5 x 3.5 inch ratio as possible
+## OUTPUT
+- .jpg file as close to the 1.4:1 aspect ratio as possible
 
-### SOURCES:
+### SOURCES
 - https://pillow.readthedocs.io/en/stable/reference/Image.html
 - https://www.mathworks.com/help/images/image-coordinate-systems.html
--- main.py
+++ main.py
@@ -1,168 +1,233 @@
 '''
-A basic script that takes in a .jpg file and crops it closer to the 2.5 x 3.5 inch ratio of a trading card.
-It considers whitespace along the image border for what to remove when cropping.
+A basic script that crops .jpg files to be as close as possible to the 1.4:1 aspect ratio of trading cards.
+It considers whitespace along the image border for what to remove (this is a specific use case for my project).
 
-INPUT:
-- .jpg file
-- height range: between 709 and 777
-- width range: 1023 or 1024
+INPUT
+    - .jpg file
+    - height range: between 709 and 777
+    - width range: 1023 or 1024
 
-OUTPUT:
-- .jpg file as close to the 2.5 x 3.5 inch ratio as possible
+OUTPUT
+    - .jpg file as close to the 1.4:1 aspect ratio as possible
 
-SOURCES:
-- https://pillow.readthedocs.io/en/stable/reference/Image.html
-- https://www.mathworks.com/help/images/image-coordinate-systems.html
+SOURCES
+    - https://pillow.readthedocs.io/en/stable/reference/Image.html
+    - https://www.mathworks.com/help/images/image-coordinate-systems.html
 '''
 
 import os
 from PIL import Image
 
 
-# resizes all images in the /input directory to the 2.5 x 3.5 inch ratio
-def resize_images():
+class ResizeImage:
 
-    project_path = os.path.dirname(os.path.abspath(__file__))
-    input_path = f'{project_path}\input'
-    output_path = f'{project_path}\output'
+    def __init__(self):
 
-    aspect_ratio = 1.4
+        # constants
+        self.aspect_ratio = 1.4
+        self.color_threshold = (100, 100, 100)
+        self.pixels_in_a_row = 3
 
-    for image in os.listdir(input_path):
+        # file paths
+        self.project_path = os.path.dirname(os.path.abspath(__file__))
+        self.input_path = f'{self.project_path}\input'
+        self.output_path = f'{self.project_path}\output'
 
-        image_path = f'{input_path}\{image}'
+        # file data
+        self.image = None
+        self.pixel_image = None
+
+        # image data
+        self.width = 0
+        self.height = 0
+        self.width_ratio = 0
+        self.height_ratio = 0
+
+        # image pixel data
+        self.x = 0
+        self.y = 0
+        self.left_x = 0
+        self.left_y = 0
+        self.right_x = 0
+        self.right_y = 0
+        self.top_x = 0
+        self.top_y = 0
+        self.bottom_x = 0
+        self.bottom_y = 0
+
+        # image crop data
+        self.left_remove = 0
+        self.right_remove = 0
+        self.top_remove = 0
+        self.bottom_remove = 0
+        self.new_width = 0
+        self.new_height = 0
+        self.current_ratio = 0
+        self.candidate = []
+    
+    def set_ratios(self):
+        """Set ratio of width or height based on aspect ratio.
+
+        e.g. 2.5 x 3.5 ratio:  1 / 1.4 = width / height
+
+        width = height / 1.4
+        height = 1.4 * width
+        """
+
+        self.width_ratio = round(self.height / self.aspect_ratio)
+
+        if(self.width_ratio > self.width):
+            self.height_ratio = round(self.aspect_ratio * self.width)
+            self.width_ratio = self.width
+        else:
+            self.height_ratio = self.height
+    
+    def find_left_pixels(self, pixel_range):
+        """Find the number of pixels to remove from the left of the image."""
+
+        black_pixels = 0
+        for num in range(pixel_range):
+            color = self.pixel_img[self.left_x + num, self.left_y]
+            if self.color_threshold > color:
+                black_pixels += 1
+                if black_pixels > self.pixels_in_a_row:
+                    self.left_remove = num - self.pixels_in_a_row
+                    break
+            else:
+                black_pixels = 0
+
+    def find_right_pixels(self, pixel_range):
+        """Find the number of pixels to remove from the right of the image."""
+
+        black_pixels = 0
+        for num in range(pixel_range):
+            color = self.pixel_img[self.right_x - num, self.right_y]
+            if self.color_threshold > color:
+                black_pixels += 1
+                if black_pixels > self.pixels_in_a_row:
+                    self.right_remove = num - self.pixels_in_a_row
+                    break
+            else:
+                black_pixels = 0
+
+    def find_top_pixels(self, pixel_range):
+        """Find the number of pixels to remove from the top of the image."""
+
+        black_pixels = 0
+        for num in range(pixel_range):
+            color = self.pixel_img[self.top_x, self.top_y + num]
+            if self.color_threshold > color:
+                black_pixels += 1
+                if black_pixels > self.pixels_in_a_row:
+                    self.top_remove = num - self.pixels_in_a_row
+                    break
+            else:
+                black_pixels = 0
+
+    def find_bottom_pixels(self, pixel_range):
+        """Find the number of pixels to remove from the bottom of the image."""
+
+        black_pixels = 0
+        for num in range(pixel_range):
+            color = self.pixel_img[self.bottom_x, self.bottom_y - num]
+            if self.color_threshold > color:
+                black_pixels += 1
+                if black_pixels > self.pixels_in_a_row:
+                    self.top_remove = num - self.pixels_in_a_row
+                    break
+            else:
+                black_pixels = 0
+
+    def loop_images(self):
+        """Loop through .jpg images in the /input directory & crop them."""
+
+        for self.image in os.listdir(self.input_path):
+
+            image_path = f'{self.input_path}\{self.image}'
+            self.crop_image(image_path)
+    
+    def crop_image(self, image_path):
+        """Crop image as close to the aspect ratio as possible by removing surrounding whitespace.
+        
+        cartesian pixel coordinate system:
+            (0,0)  ___________ (x, 0)
+                  |           |
+                  |           |
+                  |___________|
+            (0, y)             (x, y)
+        """
 
         with Image.open(image_path) as img:
 
-            # NOTE: images with landscape orientation will be rotated 90 degrees clockwise
-            # width is left/right, height is up/down
-            width, height = img.size
+            self.width, self.height = img.size
+            self.set_ratios()
 
-            # e.g. 4:3 ratio || 1 / 1.33 = width / height
-            #   width = height / 1.33
-            #   height = 1.33 * width
-            width_ratio = round(height / aspect_ratio)
+            self.pixel_img = img.load()
 
-            # set height & width to 1.4 ratio based on smaller side
-            if(width_ratio > width):
-                height_ratio = round(aspect_ratio * width)
-                width_ratio = width
-            else:
-                height_ratio = height
+            self.x = self.width - 1
+            self.y = self.height - 1
 
-            '''
-            cartesian pixel coordinate system:
-                - top left is (0,0)
-                - pixel indices are [y, x]
+            self.left_x, self.left_y = [0, round(self.y / 2)]
+            self.right_x, self.right_y = [self.x, round(self.y / 2)]
+            self.top_x, self.top_y = [round(self.x / 2), 0]
+            self.bottom_x, self.bottom_y = [round(self.x / 2), self.y]
 
-                (0,0) ___________ (x, 0)
-                    |           |
-                    |           |
-                    |___________|
-                (0, y)             (x, y)
-            '''
+            self.find_left_pixels(round(self.height / 8))
+            self.find_right_pixels(round(self.height / 8))
+            self.find_top_pixels(round(self.width / 8))
+            self.find_bottom_pixels(round(self.width / 8))
 
-            pixel_img = img.load()
-            x = width - 1
-            y = height - 1
+            self.current_ratio = self.height / self.width
+            below_aspect_ratio = self.aspect_ratio > self.current_ratio
 
-            left_x, left_y = [0, round(y / 2)]
-            right_x, right_y = [x, round(y / 2)]
-            top_x, top_y = [round(x / 2), 0]
-            bottom_x, bottom_y = [round(x / 2), y]
+            self.candidate = []
 
-            threshold = (100, 100, 100)
+            for l in range(self.left_remove + 1):
+                for r in range(self.right_remove + 1):
 
-            left_remove = 0
-            right_remove = 0
-            top_remove = 0
-            bottom_remove = 0
+                    self.new_width = self.width - l - r
 
-            black_pixels = 0
-            pixels_in_a_row = 3
+                    for t in range(self.top_remove + 1):
+                        for b in range(self.bottom_remove + 1):
 
-            for num in range(round(height / 8)):
-                color = pixel_img[left_x + num, left_y]
-                if threshold > color:
-                    black_pixels += 1
-                    if black_pixels > pixels_in_a_row:
-                        left_remove = num - pixels_in_a_row
-                        break
-                else:
-                    black_pixels = 0
-            black_pixels = 0
+                            self.new_height = self.height - b - t
+                            new_ratio = self.new_height / self.new_width
 
-            for num in range(round(height / 8)):
-                color = pixel_img[right_x - num, right_y]
-                if threshold > color:
-                    black_pixels += 1
-                    if black_pixels > pixels_in_a_row:
-                        right_remove = num - pixels_in_a_row
-                        break
-                else:
-                    black_pixels = 0
-            black_pixels = 0
+                            if below_aspect_ratio:
+                                if new_ratio > self.current_ratio and new_ratio <= self.aspect_ratio:
+                                    self.current_ratio = new_ratio
+                                    self.candidate = [l, r, t, b]
+                            else:
+                                if new_ratio < self.current_ratio and new_ratio >= self.aspect_ratio:
+                                    self.current_ratio = new_ratio
+                                    self.candidate = [l, r, t, b]
+            
+            self.save_image(img)
 
-            for num in range(round(width / 8)):
-                color = pixel_img[top_x, top_y + num]
-                if threshold > color:
-                    black_pixels += 1
-                    if black_pixels > pixels_in_a_row:
-                        top_remove = num - pixels_in_a_row
-                        break
-                else:
-                    black_pixels = 0
-            black_pixels = 0
+    def save_image(self, img):
+        """Save image to a .jpg file in the /output directory, regardless of if it was cropped / not."""
 
-            for num in range(round(width / 8)):
-                color = pixel_img[bottom_x, bottom_y - num]
-                if threshold > color:
-                    black_pixels += 1
-                    if black_pixels > pixels_in_a_row:
-                        bottom_remove = num - pixels_in_a_row
-                        break
-                else:
-                    black_pixels = 0
-            black_pixels = 0
+        if self.candidate:
 
-            new_width = 0
-            new_height = 0
-            smallest_ratio = height / width
-            candidate = []
+            l, r, t, b = self.candidate
+            self.new_width = self.width - l - r
+            self.new_height = self.height - b - t
 
-            for l in range(left_remove + 1):
-                for r in range(right_remove + 1):
+            left = l
+            top = t
+            right = self.width - r
+            bottom = self.height - b
 
-                    new_width = width - l - r
+            ratio_before = self.height / self.width
+            ratio_after = self.new_height / self.new_width
 
-                    for t in range(top_remove + 1):
-                        for b in range(bottom_remove + 1):
+            img = img.crop((left, top, right, bottom))
+            print(f'changed {self.image} | {ratio_before:.4f} -> {ratio_after:.4f}')
 
-                            new_height = height - b - t
-                            new_ratio = new_height / new_width
-
-                            if new_ratio < smallest_ratio and new_ratio >= aspect_ratio:
-                                smallest_ratio = new_ratio
-                                candidate = [l, r, t, b]
-
-            if candidate:
-                l, r, t, b = candidate
-                new_width = width - l - r
-                new_height = height - b - t
-
-                left = l
-                top = t
-                right = width - r
-                bottom = height - b
-
-                ratio_before = height / width
-                ratio_after = new_height / new_width
-
-                img = img.crop((left, top, right, bottom))
-                print(f'reduced {image} | {ratio_before:.4f} -> {ratio_after:.4f}')
-
-            img.save(f'{output_path}\{image}')
+        img.save(f'{self.output_path}\{self.image}')
 
 
 if __name__ == '__main__':
-    resize_images()
+
+    image = ResizeImage()
+    image.loop_images()
